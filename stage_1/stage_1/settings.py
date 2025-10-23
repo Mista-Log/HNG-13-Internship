fcb82db7cdcfcf859f1bef3468617e5f273bfa9c
Mista-Log <oloyedeibrahimsmile@gmail.com>
2025-10-23 08:48:52 +0100

    new commit

--- stage_1/stage_1/settings.py
+++ stage_1/stage_1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oh^230fnsu1!mpvqjelnuv+4iv*ukrbx_^@g8pnqck!nunvl0e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'stage_1.urls'
